08b384aa41eddeb5c78ed75e2984a944ba07da23
Jeremy <jeremy.spewock@gmail.com>
2020-12-09 16:13:59 -0500

    added a show route for a single post

--- resources/rants.py
+++ resources/rants.py
@@ -3,8 +3,9 @@ import models
 from flask import Blueprint, jsonify, request
 from playhouse.shortcuts import model_to_dict 
 
-rants = Blueprint('rantz', 'rant')
+rants = Blueprint('rantz', 'rant', url_prefix='rantz')
 
+#index
 @rants.route('/', methods=["GET"])
 def get_all_rants():
   try:
@@ -14,6 +15,7 @@ def get_all_rants():
   except models.DoesNotExist:
     return jsonify(data={}, status={"code": 401, "message": "error getting the data"})
 
+#create
 @rants.route('/', methods=["POST"])
 def create_rant():
   payload = request.get_json()
@@ -21,3 +23,10 @@ def create_rant():
   new_rant = models.Rants.create(**payload)
   rant_dict = model_to_dict(new_rant)
   return jsonify(data=rant_dict, status={"code": 200, "message": "success"})
+
+#show
+@rants.route('/<id>', methods=["GET"])
+def get_one_rant(id):
+  rant = models.Rants.get_by_id(id)
+  print(rant.__dict__)
+  return jsonify(data=model_to_dict(rant), status={"code": 200, "message": "success"})
